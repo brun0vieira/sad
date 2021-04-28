a32f6756e31400a9fa90a83218a205d12ad196dd
nikitadyskin <nikitadyskin94@gmail.com>
2021-04-28 17:04:14 +0100

    [PIC24] move left and right leds and buttons working

--- Template/main.c
+++ Template/main.c
@@ -11,69 +11,90 @@ _CONFIG1( JTAGEN_OFF & GCP_OFF & GWRP_OFF & COE_OFF & FWDTEN_OFF & ICS_PGx2)
 _CONFIG2( FCKSM_CSDCMD & OSCIOFNC_OFF & POSCMOD_HS & FNOSC_PRI)
 #endif
 
+void configPorts();
+void setNormal();
+void setStandby();
+int changeMode(int state);
+void delay(int t);
+void moveRight();
+void moveLeft();
+void stop();
+
+void configPorts() {
+	TRISDbits.TRISD6 = 1; // Button to change mode (Standby and normal)
+	TRISAbits.TRISA7 = 1; // Button to move right
+	TRISAbits.TRISA6 = 0; // LED to indicate if the motor is ON or OFF
+	TRISAbits.TRISA0 = 0; // Motor is moving right
+	TRISAbits.TRISA1 = 0; // Motor is moving left
+	TRISDbits.TRISD7 = 1; // Button to move left
+	TRISAbits.TRISA7 = 1; // Button to move right
+}
+
+void setNormal() 
+{
+	PORTAbits.RA6 = 1;
+}
+
+void setStandby() 
+{
+	stopMotor();
+	PORTAbits.RA6 = 0;
+}
+
+int changeMode(int state) // check debounce 
+{
+	state = !state;
+	
+	if(state)
+		setNormal();
+	else
+		setStandby();
+
+	return state;
+}
+
+void delay(int t)
+{
+    int i;
+    for(i=0;i<t;i++);
+}
+
+void moveRight()
+{
+	LATAbits.LATA1 = 0;
+	LATAbits.LATA0 = 1;
+}
+
+void moveLeft()
+{
+	LATAbits.LATA0 = 0;
+	LATAbits.LATA1 = 1;
+}
+
+void stopMotor() 
+{
+	LATAbits.LATA0 = 0;
+	LATAbits.LATA1 = 0;
+}
 
 int main(void)
-{
-	TRISDbits.TRISD6 = 1;
-	TRISAbits.TRISA0 = 0;
-	TRISAbits.TRISA1 = 0;
-	TRISAbits.TRISA2 = 0;
-	TRISAbits.TRISA3 = 0;
-	TRISAbits.TRISA4 = 0;
-	TRISAbits.TRISA5 = 0;
-	TRISAbits.TRISA6 = 0;
-	TRISAbits.TRISA7 = 0;
-
-	int i = 0;
-	int j = 0;
-
-	while ( 1 ){
-		if ( !PORTDbits.RD6 ){
-			/*PORTAbits.RA0 = 1;
-			PORTAbits.RA1 = 1;
-			PORTAbits.RA2 = 1;
-			for( i = 0 ; i < 20000 ; i++){};
-			PORTAbits.RA0 = 0;
-			PORTAbits.RA1 = 0;
-			PORTAbits.RA2 = 0;
-			for( i = 0 ; i < 20000 ; i++){};*/
-			for( j = 0; j<3; j++)
-			{
-				PORTAbits.RA0 = 1;
-					for( i = 0 ; i < 20000 ; i++){};
-					PORTAbits.RA1 = 1;
-					for( i = 0 ; i < 20000 ; i++){};
-					PORTAbits.RA2 = 1;	
-					for( i = 0 ; i < 20000 ; i++){};
-					PORTAbits.RA3 = 1;
-					for( i = 0 ; i < 20000 ; i++){};
-					PORTAbits.RA4 = 1;
-					for( i = 0 ; i < 20000 ; i++){};
-					PORTAbits.RA5 = 1;
-					for( i = 0 ; i < 20000 ; i++){};
-					PORTAbits.RA6 = 1;
-					for( i = 0 ; i < 20000 ; i++){};
-					PORTAbits.RA7 = 1;
-					for( i = 0 ; i < 20000 ; i++){};
-					PORTAbits.RA7 = 0;
-					for( i = 0 ; i < 20000 ; i++){};
-					PORTAbits.RA6 = 0;
-					for( i = 0 ; i < 20000 ; i++){};
-					PORTAbits.RA5 = 0;	
-					for( i = 0 ; i < 20000 ; i++){};
-					PORTAbits.RA4 = 0;
-					for( i = 0 ; i < 20000 ; i++){};
-					PORTAbits.RA3 = 0;
-					for( i = 0 ; i < 20000 ; i++){};
-					PORTAbits.RA2 = 0;
-					for( i = 0 ; i < 20000 ; i++){};
-					PORTAbits.RA1 = 0;
-					for( i = 0 ; i < 20000 ; i++){};
-					PORTAbits.RA0 = 0;
-				
-				
-			}
-			
-		}
+{	
+	int state = 0;
+	configPorts();
+	
+	while(1)
+	{
+		delay(20);
+		if(!PORTDbits.RD6)
+        {
+            state = changeMode(state);
+        }
+		if(state)
+		{
+			if(!PORTDbits.RD7)
+				moveLeft();
+			if(!PORTAbits.RA7)
+				moveRight();
+		}			
 	}
 }
